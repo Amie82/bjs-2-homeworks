2f35bb3a16d10a735b17455e509c3dcad4ed46f6
Zhelnina <amie82@list.ru>
2022-06-15 21:55:30 +0700

    First HomeWork_first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,43 @@
+'use strict';
+let arr = [];
+let finArr = [];
+let a = 1;
+let b = 5;
+let c = 2;
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  
+  arr = b**2-4*a*c;
+  
+// код для задачи
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
+arr = solveEquation(a, b, c);
+console.log('arr:' + arr);
 
-  return totalAmount;
+
+if(arr < 0) {
+   console.log('Корней нет, пустой массив:' + arr);
 }
+
+else if(arr === 0) {
+  finArr[0] = (-b/(2*a));
+console.log('Корень один: ' + finArr);
+}
+else {
+  finArr[0] = [(-b + Math.sqrt(arr))/(2*a)];
+  finArr[1] = [(-b - Math.sqrt(arr) )/(2*a)];
+console.log('Больше нуля: ' + finArr);
+}
+
+
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
+// 
+
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
