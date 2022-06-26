61cd27947e8c56adf05e44f8b49d9b33e0c86301
Anna Zhelnina <amie82@list.ru>
2022-06-26 20:11:49 +0700

    first commit second homework

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,51 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+     }
+     avg = parseFloat((sum / arr.length).toFixed(2));
+     return { min: min, max: max, avg: avg };
 }
 
+
+
+
+
 // Задание 2
+arrOfArr = [[1, 2, 3, 4],
+           [10, 20, -10, -20]];
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
+function makeWork(arrOfArr, function worker) {
   let max;
-
-  // Ваш кода
-  // for ...
+   for (let i = 0; i < arrOfArr.length; i++) {
+    let arrSum = function(arrOfArr[i]);
+    if (arrSum > max) {
+      max = arrSum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return getArrayParams(arr).max - getArrayParams(arr).min;
 }
