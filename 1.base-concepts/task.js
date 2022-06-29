0bc294e5eb8a899f21399127e7805faeb153e82d
Anna Zhelnina <amie82@list.ru>
2022-06-29 11:00:31 +0700

    fourth commit first homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,19 @@
 'use strict';
-let arr = [];
-let discriminant;
 
 function solveEquation(a, b, c) {
-  
+  let arr = [];
+  let discriminant;
   discriminant = b**2-4*a*c;
   if(discriminant === 0) {
     arr[0] = (-b/(2*a));
     }
-  else {
+  else if (discriminant > 0) {
     arr[0] = (-b + Math.sqrt(discriminant))/(2*a);
     arr[1] = (-b - Math.sqrt(discriminant) )/(2*a);
     } 
   return arr; // array
 
 }
-
-
-solveEquation(2, 7, 4);
-console.log('arr:' + discriminant);
 
 
 
