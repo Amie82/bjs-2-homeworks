0175215599765acbab5650aac22b905d8423d4f7
Anna Zhelnina <amie82@list.ru>
2022-06-29 19:06:11 +0700

    first commit third homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
+'user strick'
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = (arr1.length === arr2.length) && arr1.every((v, i) => arr2[i] == v);
+  
   return result; // boolean
 }
+
+
+
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
 
   return resultArr; // array
 }
